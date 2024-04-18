67643393878260255b2cc058c700028da0998dee
CooperPeaks <jbayard269@gmail.com>
2024-04-18 15:08:59 +0200

    update story content with chapter

--- api/controllers/chapterController.js
+++ api/controllers/chapterController.js
@@ -1,4 +1,5 @@
-const Chapter = require('../models/chapterModel')
+const Chapter = require('../models/chapterModel');
+const Story = require('../models/storyModel');
 const User = require('../models/userModel')
 const { validationResult } = require('express-validator')
 
@@ -10,13 +11,17 @@ module.exports = {
         }
         else {
             const user = await User.findOne({ where: { username: req.session.username } }, { raw: true })
-            await Chapter.create({
+            const chapter = await Chapter.create({
                 content: req.body.content,
                 storyId: req.params.storyId,
                 userId: user.id
             })
-            console.log(req.body, Chapter.userId);
+            const existingStory = await Story.findByPk(req.params.storyId)
+            const updatedContent = existingStory.content + `\n` + chapter.content;
+            await Story.update({ content: updatedContent }, { where: { id: req.params.storyId } })
+            console.log(req.body, chapter.userId, updatedContent, existingStory);
         }
         res.redirect('back')
     }
 }
+
--- api/models/chapterModel.js
+++ api/models/chapterModel.js
@@ -1,4 +1,4 @@
-const {DataTypes} = require('sequelize')
+const { DataTypes } = require('sequelize')
 const config = require('../../config')
 const Story = require('./storyModel')
 
